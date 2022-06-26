b39fabb6ff379d868656e818ea843ebd86944e2c
Nishad Islam <nishad.nik@gmail.com>
2022-06-26 22:52:16 +0600

    image overlay added

--- src/components/Thumbnail.tsx
+++ src/components/Thumbnail.tsx
@@ -6,7 +6,16 @@ interface Props {
 }
 function Thumbnail({ movie }: Props) {
   return (
-    <img className='mx-2' src={imageUrl + movie.poster_path} alt='Movie'></img>
+    <div className='image-container'>
+      <img
+        className='mx-2'
+        src={imageUrl + movie.poster_path}
+        alt='Movie'
+      ></img>
+      <div className='overlay d-flex align-items-center justify-content-center'>
+        Add To Favorite
+      </div>
+    </div>
   )
 }
 
