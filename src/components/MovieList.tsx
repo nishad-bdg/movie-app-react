ba9c9cacb84de35a61d97fd3d041417754f51f85
Nishad Islam <nishad.nik@gmail.com>
2022-07-12 17:31:05 +0600

    dynamic horizontal scroll added

--- src/components/MovieList.tsx
+++ src/components/MovieList.tsx
@@ -1,5 +1,7 @@
+import { useRef, useState } from 'react'
 import { Movie, Movies } from '../../typings'
 import Thumbnail from './Thumbnail'
+import { ChevronLeftIcon, ChevronRightIcon } from '@heroicons/react/outline'
 
 interface Props {
   title: string
@@ -7,18 +9,40 @@ interface Props {
   addToFavoriteClick?: (movie: Movie) => void
 }
 function MovieList({ title, movies, addToFavoriteClick }: Props) {
+  const rowRef = useRef<HTMLDivElement>(null)
+  const [isMoved, setIsMoved] = useState<boolean>(false)
+
+  const handleClick = (direction: string) => {
+    setIsMoved(true)
+    if (rowRef.current) {
+      const { scrollLeft, clientWidth } = rowRef.current
+      const scrollTo =
+        direction === 'left'
+          ? scrollLeft - clientWidth
+          : scrollLeft + clientWidth
+      rowRef.current.scrollTo({ left: scrollTo, behavior: 'smooth' })
+      if (scrollLeft <= 0) {
+        setIsMoved(false)
+      }
+    }
+  }
+
   return (
     <div className='container-fluid movie-list mt-5'>
       <h2>{title}</h2>
-      <div className='d-flex flex-nowrap'>
-        {movies.map((movie: Movie) => (
-          <Thumbnail
-            key={movie.id}
-            movie={movie}
-            title={title}
-            addToFavoriteClick={(e) => addToFavoriteClick?.(movie)}
-          />
-        ))}
+      <div>
+        <ChevronLeftIcon className={`left-icon ${ !isMoved && 'hide-element'}`} onClick={() => handleClick('left')} />
+        <div className='d-flex flex-nowrap' ref={rowRef}>
+          {movies.map((movie: Movie) => (
+            <Thumbnail
+              key={movie.id}
+              movie={movie}
+              title={title}
+              addToFavoriteClick={(e) => addToFavoriteClick?.(movie)}
+            />
+          ))}
+        </div>
+        <ChevronRightIcon className='right-icon' onClick={() => handleClick('right')} />
       </div>
     </div>
   )
